1483563bb1f0bd5301336679ef1cbc7cb9f9c1b1
Xico <francisco100eg@gmail.com>
2022-11-11 20:27:36 -0300

    Feat: Added 3D image

--- trybe-market/src/Pages/Login/Login.jsx
+++ trybe-market/src/Pages/Login/Login.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas} from "@react-three/fiber"
+import {OrbitControls} from "@react-three/drei"
+import {Model} from '../../components/Store'
+import './Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <div className='Login__container'>
+        <div className='Login__form'>
+          <h1>Enter</h1>
+        </div>
+      <Canvas className='canvas'>
+        <OrbitControls enableZoom={false}/>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2,5,2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model/>
+          </Suspense>
+      </Canvas>
+      </div>
+    </div>
   )
 }
 
