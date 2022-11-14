5dd90b2e67184f7bb1e9454ebcf63099eee33b13
Xico <francisco100eg@gmail.com>
2022-11-14 17:53:18 -0300

    Feat: New routes (Shopping cart / Payment / NotFound)

--- trybe-market/src/App.js
+++ trybe-market/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import {Login, Homepage, Profile, Signup, Forgotpassword, ProductDetails} from './Pages'
+import {Login, Homepage, Profile, Signup, Forgotpassword, ProductDetails, ShoppingCart, Payment, PageNotFound} from './Pages'
 
 function App() {
   return (
@@ -13,6 +13,10 @@ function App() {
       <Route exact path='/productDetails/:id' element={ <ProductDetails />} />
       <Route exact path='/forgotpassword' element = { <Forgotpassword/>} />
       <Route exact path="/profile" element={ <Profile/> } />
+      <Route exact path="/shoppingCart" element={ <ShoppingCart/> } />
+      <Route exact path="/payment" element={ <Payment /> } />
+      <Route path='*' element ={<PageNotFound/>} />
+
     </Routes>
     </div>
   );
--- trybe-market/src/Pages/index.js
+++ trybe-market/src/Pages/index.js
@@ -4,8 +4,10 @@ import Profile from './Profile/Profile';
 import Signup from './Signup/Signup';
 import Forgotpassword from './Forgotpassword/Forgotpassword';
 import ProductDetails from './productDetails/ProductDetails';
-
+import Payment from './Payment/Payment';
+import ShoppingCart from './ShoppingCart/ShoppingCart';
+import PageNotFound from './PageNotFound/PageNotFound';
 
 export{
-    Login, Homepage, Profile, Signup, Forgotpassword,ProductDetails
+    Login, Homepage, Profile, Signup, Forgotpassword,ProductDetails, Payment, ShoppingCart, PageNotFound
 }
