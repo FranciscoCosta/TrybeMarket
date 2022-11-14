0fefbd3e06f34bf20dc93b56085a5a60a58a2bf7
Xico <francisco100eg@gmail.com>
2022-11-14 18:22:01 -0300

    Feat: Page Not Found

--- trybe-market/src/Pages/PageNotFound/PageNotFound.jsx
+++ trybe-market/src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import NOTFOUND from '../../Assets/404.jpg'
+import './PageNotFound.css'
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate();
   return (
-    <div>PageNotFound</div>
+    <div className='PageNotFound'>
+      <div className='PageNotFound__container'>
+        <div className='PageNotFound__img-container'>
+          <img src={NOTFOUND} alt='PageNotFound 404'/>
+        </div>
+        <button
+        type='button'
+        className='PageNotFound__home-btn'
+        onClick={()=> navigate("/home")}
+        >
+          Home
+        </button>
+      </div>
+    </div>
   )
 }
 
-export default PageNotFound
+export default PageNotFound;
--- trybe-market/src/Pages/Payment/Payment.jsx
+++ trybe-market/src/Pages/Payment/Payment.jsx
@@ -6,4 +6,4 @@ function Payment() {
   )
 }
 
-export default Payment
+export default Payment;
