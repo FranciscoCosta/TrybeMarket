83e53f4b117bc80d29f4f46bef12104012f3bdd7
Xico <francisco100eg@gmail.com>
2022-11-16 14:34:00 -0300

    Feat: Page of Shopping Cart finish

--- trybe-market/src/components/CartItems/CartItems.jsx
+++ trybe-market/src/components/CartItems/CartItems.jsx
@@ -1,34 +1,97 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useContext } from "react";
 import "./CartItems.css";
-
+import { Context } from "../../Context/Context";
+import { Link } from "react-router-dom";
 function CartItems() {
   const [cartItemsAdded, setcartItemsAdded] = useState([]);
+  const {handleTotalCart} = useContext(Context);
+  const [totalValue, settotalValue] = useState(0)
   useEffect(() => {
     handleCartItems();
-  }, []);
+    handleTotalValue();
+  }, [handleTotalCart]);
 
   const handleCartItems = () => {
     const cart = JSON.parse(localStorage.getItem("cartItems")) || [];
     setcartItemsAdded(cart);
+    
   };
-  const handleRemoveItem=(event)=>{
-    console.log(event)
+  const handleRemoveItem=({target : {value}})=>{
+    const local = JSON.parse(localStorage.getItem("cartItems"));
+    const result = local.filter((item)=> item.id !== value)
+    console.log(result)
+    localStorage.setItem('cartItems', JSON.stringify(result));
+    handleTotalCart();
+  } 
+
+
+
+ const handleTotalValue = () => {
+  if(cartItemsAdded.length !== 0){
+  const priceTotal =  cartItemsAdded.map((item)=>item.price * item.quantity).reduce((acc,curr)=>acc+curr)
+  settotalValue(parseFloat(priceTotal).toFixed(2))
+  }
+ }
+
+ const handleIncrement=({target : {value}})=>{
+  const updateItemQuantity = cartItemsAdded.map((item)=>{
+    if(item.id === value && item.quantity <item.available_quantity){
+      item['quantity'] += 1
+      return item
+    }else{
+      return item
+    }
+  })
+  setcartItemsAdded(updateItemQuantity);
+  localStorage.setItem('cartItems', JSON.stringify(updateItemQuantity));
+  handleTotalValue();
+  handleTotalCart();
+}
+
+const handleDecrement=({target : {value}})=>{
+  const updateItemQuantity = cartItemsAdded.map((item)=>{
+    if(item.id === value && item.quantity >1){
+      item['quantity'] -= 1
+      return item
+    }else{
+      return item
+    }
+  })
+  setcartItemsAdded(updateItemQuantity);
+  localStorage.setItem('cartItems', JSON.stringify(updateItemQuantity));
+  handleTotalValue();
+  handleTotalCart();
+}
+
+  const handleRemoveAll=()=>{
+    localStorage.setItem('cartItems', JSON.stringify([]));
+    handleTotalValue();
+    handleTotalCart();
   }
 
   return (
     <div className="CartItems">
-      {cartItemsAdded.length > 0 && (
+      {cartItemsAdded.length > 0 ? (
         <div className="CartItems__container">
           <div className="CartItems__right">
-            <h4 className="CartItems__total">Total :</h4>
-            <button type="button" className="CartItems__finish-btn">
+            <button
+            className="CartItems__removeAllbtn CartItems-btn"
+            onClick={handleRemoveAll}
+            >
+              Limpar Carrinho
+            </button>
+            <h4 className="CartItems__total">Total : R$ {totalValue}</h4>
+            <Link to={"/payment"}>
+            <button type="button" className="CartItems__finish-btn CartItems-btn">
               Finalizar compra
             </button>
+
+            </Link>
           </div>
           <div className="CartItems__left">
             {cartItemsAdded.map((item) => (
               <div className="CartItem__item-card" key={item.id}>
-                <div className="ProductDetails__quantity ">
+                <div className="ProductDetails__quantity remove-btn">
                 <button type="button"
                 value={item.id}
                 onClick={handleRemoveItem}
@@ -37,29 +100,42 @@ function CartItems() {
                 </button>
                 </div>
                 <div className="CartItems__img">
-                  <img src={item.thumbnail} alt={item.tile} />
+                  <img src={item.thumbnail} alt={item.title} />
                 </div>
                 <h4 className="CartItems__title">{item.title}</h4>
+                <div className="CartItems__buttons">
 
+                
                 <div className="ProductDetails__quantity">
-                  <button type="button">-</button>
+                  <button
+                  onClick={handleDecrement}
+                  value={item.id}
+                  type="button">-</button>
                 </div>
                 <input
                   min="1"
                   className="ProductDetails__quantity-input"
                   type="number"
+                  value={item.quantity}
                 ></input>
                 <div className="ProductDetails__quantity">
-                  <button type="button">+</button>
+                  <button
+                  onClick={handleIncrement}
+                  value={item.id}
+                  type="button">+</button>
                 </div>
-                <h4 className="CartItems__price">Preço: </h4>
-                <h4 className="CartItems__unities">Unidades:</h4>
-                <h4 className="CartItems__price-total">Total: </h4>
+                </div>
+                <div className="CartItems__details">
+                <h4 className="CartItems__info">Preço:<span> R${item.price}</span></h4>
+                <h4 className="CartItems__info">Unidades: <span>{item.quantity}</span></h4>
+                <h4 className="CartItems__info">Total: <span> R${parseFloat((item.price*item.quantity)).toFixed(2)}</span></h4>
+                </div>
               </div>
             ))}
           </div>
         </div>
-      )}
+      )
+      : <p>Não possui items no carrinho </p>}
     </div>
   );
 }
