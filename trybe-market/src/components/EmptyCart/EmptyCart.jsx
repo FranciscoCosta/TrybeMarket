2cbe17e8efdf5d1608f6db08ed261b7dcc1e40c7
Xico <francisco100eg@gmail.com>
2022-11-21 17:29:40 -0300

    Fix:Remove emptycart Img

--- trybe-market/src/components/EmptyCart/EmptyCart.jsx
+++ trybe-market/src/components/EmptyCart/EmptyCart.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './EmptyCart.css'
-import emptyCartpng from '../../Assets/EmpetyCart.png'
 import { FaSadCry} from "@react-icons/all-files/fa/FaSadCry";
 
 function EmptyCart() {
@@ -8,9 +7,6 @@ function EmptyCart() {
     <div className='EmptyCart'>
         <div className='EmptyCart__container'>
             <h1 className='EmptyCart__title'>Não possui itens no seu carrinho <FaSadCry size={50}/>.</h1>
-            <div className="EmptyCart__img-container">
-                <img src={emptyCartpng} alt="emptyCart png"/>
-            </div>
         </div>
 
     </div>
